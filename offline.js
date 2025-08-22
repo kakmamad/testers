3cb4698a34302a52c3f5e856d9152b908c18f742
kakmamad <dm53523@gmail.com>
2025-08-22 16:22:57 +0430

    upload game

---
+++ offline.js
@@ -0,0 +1,16 @@
+﻿{
+	"version": 1755862326,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"adapter.js"
+	]
+}
